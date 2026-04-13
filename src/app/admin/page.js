7d3c63cc3f8da2c9cd6b4f20201dda093ebfecf4
DHKIM-17 <a27301688@gmail.com>
2026-04-14 03:49:53 +0900

    fix: add missing await in AdminPage

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -4,8 +4,8 @@ import AdminDashboardClient from './AdminDashboardClient'
 export const dynamic = 'force-dynamic'
 
 export default async function AdminPage() {
-  const photos = photoDb.findMany()
-  const articles = articleDb.findMany()
+  const photos = await photoDb.findMany()
+  const articles = await articleDb.findMany()
   
   return <AdminDashboardClient initialPhotos={photos} initialArticles={articles} />
 }
